93c8e813c8dcd6c1375cb526ae2e7ccadb31eea6
Carlos Rodrigues <cefrodrigues@gmail.com>
2014-02-09 15:09:31 +0000

    Update README. Bump version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     maintainer_email='cefrodrigues@gmail.com',
     name='python-kyototycoon',
     description='Kyoto Tycoon Client Library',
-    version='0.5.5',
+    version='0.5.6',
     license='BSD',
     keywords='Kyoto Tycoon, Kyoto Cabinet',
     packages=['kyototycoon'],
