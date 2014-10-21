7f3d307cc63402b26a968db1466f7058183f8d88
Carlos Rodrigues <cefrodrigues@gmail.com>
2014-10-21 18:33:26 +0100

    Parse log entries and return a dictionary.

--- kyototycoon/kyotoslave.py
+++ kyototycoon/kyotoslave.py
@@ -11,8 +11,12 @@ import time
 
 from .kt_error import KyotoTycoonException
 
-MB_REPLICATION = 0xb1
-MB_SYNCED = 0xb0
+MB_REPL = 0xb1
+MB_SYNC = 0xb0
+
+OP_SET = 0xa1
+OP_REMOVE = 0xa2
+OP_CLEAR = 0xa5
 
 class KyotoSlave(object):
     def __init__(self, sid, host='127.0.0.1', port=1978, timeout=30):
@@ -24,33 +28,82 @@ class KyotoSlave(object):
         self.port = port
         self.timeout = timeout
 
-    def consume(self):
+    def consume(self, timestamp=None):
         self.socket = socket.create_connection((self.host, self.port), self.timeout)
 
-        kt_now = int(time.time() * 1000) * 10**6
-        request = [struct.pack('!BIQH', MB_REPLICATION, 0, kt_now, self.sid)]
+        start_ts = int(time.time() * 10**9) if timestamp is None else timestamp
+
+        # Ask the server for all available transaction log entries since "start_ts"...
+        request = [struct.pack('!BIQH', MB_REPL, 0x00, start_ts, self.sid)]
         self._write(b''.join(request))
 
         magic, = struct.unpack('!B', self._read(1))
-        if magic != MB_REPLICATION:
+        if magic != MB_REPL:
             raise KyotoTycoonException('bad response [%s]' % hex(magic))
 
         while True:
             magic, ts = struct.unpack('!BQ', self._read(9))
-            if magic == MB_SYNCED:
-                self._write(chr(MB_REPLICATION))
+            if magic == MB_SYNC:  # ...the head of the transaction log has been reached.
+                self._write(chr(MB_REPL))
                 continue
 
-            size, = struct.unpack('!I', self._read(4))
-            log = self._read(size)
+            if magic != MB_REPL:
+                raise KyotoTycoonException('bad response [%s]' % hex(magic))
 
-            # TODO: improve this
-            yield log
+            # Common log entry information...
+            size, = struct.unpack('!I', self._read(4))
+            sid, db, db_op = struct.unpack('!HHB', self._read(5))
+
+            if db_op == OP_CLEAR:
+                yield {"sid": sid, "db": db, "operation": "clear"}
+                continue
+
+            buf = self._read(size - 5)
+
+            if db_op == OP_REMOVE:
+                key_size, buf = self._read_varnum(buf)
+                key = buf[:key_size]
+
+                yield {"sid": sid, "db": db, "operation": "remove", "key": key}
+                continue
+
+            if db_op == OP_SET:
+                key_size, buf = self._read_varnum(buf)
+                val_size, buf = self._read_varnum(buf)
+
+                key = buf[:key_size]
+                val = buf[key_size:]
+
+                # The expiration time is contained in the value portion...
+                xt, = struct.unpack('!Q', '\x00\x00\x00' + val[:5])
+                val = val[5:]
+
+                yield {"sid": sid, "db": db, "operation": "set", "key": key, "value": val, "expire_ts": xt}
+                continue
+
+            raise KyotoTycoonException('unsupported database operation [%s]' % hex(db_op))
 
     def close(self):
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
         return True
+
+    def _read_varnum(self, data):
+        rp = 0
+        value = 0
+
+        while True:
+            if rp >= len(data):
+                return (0, data)
+
+            curr_byte = ord(data[rp])
+            value = (value << 7) + (curr_byte & 0x7f)
+            rp += 1
+
+            if curr_byte < 0x80:
+                break
+
+        return (value, data[rp:])
 
     def _write(self, data):
         self.socket.sendall(data)
